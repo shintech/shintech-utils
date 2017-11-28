d0e7c18d8e3fae95d7169d07edbe21f9290cc9d4
Michael Prather <mprather1@gmail.com>
2017-11-28 22:36:41 +0000

    status.method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import chalk from 'chalk'
 
-function getStatusCodeStyle (status) {
-  if (status === 400) {
-    return { code: chalk.red(status), level: 'error', message: '--- Not Found' }
-  } else if (status === 304) {
-    return { code: chalk.cyan(status), level: 'info', message: '' }
-  } else {
-    return { code: chalk.green(status), level: 'info', message: '' }
+function getStatusCodeStyle (status, method) {
+  var message = {}
+
+  if (method === 'GET') {
+    message['method'] = chalk.yellow(method)
+  } else if (method === 'POST') {
+    message['method'] = chalk.magenta(method)
+  } else if (method === 'PUT') {
+    message['method'] = chalk.blue(method)
   }
+
+  if (status === 400) {
+    message['code'] = chalk.red(status)
+    message['level'] = 'error'
+    message['message'] = '--- Not Found'
+  } else if (status === 304) {
+    message['code'] = chalk.cyan(status)
+    message['level'] = 'info'
+    message['message'] = ''
+  } else {
+    message['code'] = chalk.green(status)
+    message['level'] = 'info'
+    message['message'] = ''
+  }
+
+  return message
 }
 
 export {
